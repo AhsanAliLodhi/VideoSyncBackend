044da5af21d3d6dddabf5ff4c7fcab03a5c493c8
ahsanalilodhi <ahsanalilodhi@gmail.com>
2018-09-04 15:31:18 +0200

    added google signin button

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 import config
-from app.app import audi_app
+from app.app import vs_app
 
 if __name__ == '__main__':
-    audi_app.run(
+    vs_app.run(
         host='0.0.0.0',
         port=int(os.getenv('FLASK_PORT')),
         debug=os.getenv('FLASK_DEBUG')
